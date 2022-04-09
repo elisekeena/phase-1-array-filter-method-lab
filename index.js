530d3184836954c66625065815d15a9f5e1f8644
Elise <elise041294@gmail.com>
2022-04-09 14:16:02 -0600

    commit tree 0c2fa95c273a910e838033a8a2ace304e87fbf91

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching (array, string){
+    // return drivers.filter((drivers) => drivers.toUpperCase() == name.toUpperCase())
+return array.filter(function(drivers){
+    return drivers.toUpperCase() == string.toUpperCase();
+    })
+};
+
+function fuzzyMatch (array, string){
+    return array.filter(function(drivers){
+        return drivers.substring(0,string.length) == string.substring(0,string.length)
+    })
+};
+
+function matchName (obj, string){
+    return obj.filter(function(drivers){
+        return drivers.name === string
+    })
+};
+
